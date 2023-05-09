e4e89429d7c269e8e446d17e64d55a0afc7132a4
adem <a.ayar94@gmail.com>
2023-05-09 14:50:23 +0300

    added loading state YourStatsPreviewFragment

--- app/src/main/java/com/aayar94/valorantguidestats/ui/fragment/your_stats_preview/YourStatsPreviewViewModel.kt
+++ app/src/main/java/com/aayar94/valorantguidestats/ui/fragment/your_stats_preview/YourStatsPreviewViewModel.kt
@@ -7,6 +7,7 @@ import com.aayar94.valorantguidestats.data.Repository
 import com.aayar94.valorantguidestats.data.models.user_stats.last_matches.UserMatchesDataModel
 import com.aayar94.valorantguidestats.data.models.user_stats.user_cards.UserStatsMainDataModel
 import com.aayar94.valorantguidestats.data.models.user_stats.user_mmr_change.UserMMRChangeDataModel
+import com.aayar94.valorantguidestats.util.ResponseHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,28 +17,28 @@ class YourStatsPreviewViewModel @Inject constructor(
     val repository: Repository
 ) : ViewModel() {
 
-    val userMainStats = MutableLiveData<UserStatsMainDataModel?>()
-    val userMatchHistory = MutableLiveData<UserMatchesDataModel?>()
-    val userMMRChange = MutableLiveData<UserMMRChangeDataModel?>()
+    val userMainStats = MutableLiveData<ResponseHandler<UserStatsMainDataModel>>()
+    val userMatchHistory = MutableLiveData<ResponseHandler<UserMatchesDataModel>>()
+    val userMMRChange = MutableLiveData<ResponseHandler<UserMMRChangeDataModel>>()
 
     fun getUserStats(gamerTag: String, tag: String) {
         viewModelScope.launch {
             val response = repository.getUserMainStats(gamerTag, tag)
-            userMainStats.postValue(response.data)
+            userMainStats.postValue(response)
         }
     }
 
     fun getUserMatchHistory(region: String, puuid: String) {
         viewModelScope.launch {
             val matchHistoryResponse = repository.getUserMatchHistory(region, puuid)
-            userMatchHistory.postValue(matchHistoryResponse.data)
+            userMatchHistory.postValue(matchHistoryResponse)
         }
     }
 
     fun getUserMMRChange(region: String, puuid: String) {
         viewModelScope.launch {
             val mmrChangeResponse = repository.getUserMMRChange(region, puuid)
-            userMMRChange.postValue(mmrChangeResponse.data)
+            userMMRChange.postValue(mmrChangeResponse)
         }
     }
 }
