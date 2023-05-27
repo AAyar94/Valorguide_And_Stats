feeb950ab86500b4443dc48e3fbb0484a1b7d08a
Adem Ayar <79112917+AAyar94@users.noreply.github.com>
2023-05-27 19:08:38 +0300

    ui changes now handling, weapon skin video showcase component size changed

--- app/src/main/java/com/aayar94/valorantguidestats/data/models/game_content/Map.kt
+++ app/src/main/java/com/aayar94/valorantguidestats/data/models/game_content/Map.kt
@@ -8,12 +8,27 @@ data class ValorantMap(
     val uuid: String,
     val displayName: String,
     val coordinates: String,
-    val displayIcon: String?,
-    val listViewIcon: String?,
+    val displayIcon: String,
+    val listViewIcon: String,
     val splash: String,
+    val assetPath: String,
     val mapUrl: String,
     val xMultiplier: Double,
     val yMultiplier: Double,
     val xScalarToAdd: Double,
-    val yScalarToAdd: Double
+    val yScalarToAdd: Double,
+    val callouts: List<Callout>,
+) : Parcelable
+
+@Parcelize
+data class Callout(
+    val regionName: String,
+    val superRegionName: String,
+    val location: Location,
+) : Parcelable
+
+@Parcelize
+data class Location(
+    val x: Double,
+    val y: Double,
 ) : Parcelable
